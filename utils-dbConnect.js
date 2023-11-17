7daa8afd4daa255937d2be8a18c292055f025a5b
shukla307 <78009394+shukla307@users.noreply.github.com>
2023-11-17 11:03:44 +0530

    Create utils-dbConnect.js
    Fetch and display available plans:
    
    To fetch and display available plans, we can create an API route in Next.js to retrieve data from database  and then render the plans on a page:
    
    
    Set up MongoDB connection:
    >>npm install mongoose

---
+++ utils-dbConnect.js
@@ -0,0 +1,37 @@
+// utils/dbConnect.js
+
+import mongoose from 'mongoose';
+
+const MONGODB_URI = 'mongodb_uri';
+
+if (!MONGODB_URI) {
+  throw new Error('define the MONGODB_URI environment variable inside .env.local');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const options = {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, options).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
