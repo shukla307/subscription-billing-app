141680e5621e7ef2bda08a009a41fbf8d176f5d8
shukla307 <78009394+shukla307@users.noreply.github.com>
2023-11-17 11:00:34 +0530

    Create pages-auth-login.js

---
+++ pages-auth-login.js
@@ -0,0 +1,58 @@
+// pages/auth/login.js
+
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSession, signIn } from 'next-auth/react';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const handleLogin = async () => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      username,
+      password,
+    });
+
+    if (!result.error) {
+      router.push('/');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {session ? (
+        <p>You are already logged in as {session.user.name}.</p>
+      ) : (
+        <form>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      )}
+    </div>
+  );
+}
+
